70663950ec64188ed01b3baaf83a5ccf70425100
jonas <jonas.voisard@gmail.com>
2025-01-19 19:45:44 +0100

    handle -n

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,7 +6,7 @@
 /*   By: jvoisard <jonas.voisard@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 19:10:39 by jvoisard          #+#    #+#             */
-/*   Updated: 2025/01/19 19:37:11 by jvoisard         ###   ########.fr       */
+/*   Updated: 2025/01/19 19:43:36 by jvoisard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,22 @@
 int	builtin_echo(t_sh *shell, t_command *cmd)
 {
 	t_list	*arg;
+	bool	new_line;
 
 	if (!cmd->args)
 		return (1);
 	arg = cmd->args->next;
+	new_line = ft_strcmp(arg->content, "-n");
+	if (!new_line)
+		arg = arg->next;
 	while (arg)
 	{
 		ft_putstr_fd(arg->content, shell->pipe.out);
-		ft_putstr_fd(" ", shell->pipe.out);
+		if (arg->next)
+			ft_putstr_fd(" ", shell->pipe.out);
 		arg = arg->next;
 	}
-	ft_putstr_fd("\n", shell->pipe.out);
+	if (new_line)
+		ft_putstr_fd("\n", shell->pipe.out);
 	return (0);
 }
