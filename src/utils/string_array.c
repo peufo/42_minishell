28f4b54e6fb8c5b35b9b7e2cc8c627276ef5a407
jonas <jonas.voisard@gmail.com>
2025-01-21 21:57:03 +0100

    env use new string_array utils

---
+++ src/utils/string_array.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   string_array.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvoisard <jonas.voisard@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/21 20:05:13 by jvoisard          #+#    #+#             */
+/*   Updated: 2025/01/21 21:50:19 by jvoisard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "string_array.h"
+
+int	string_array_len(char **arr)
+{
+	int	index;
+
+	index = 0;
+	while (arr[index])
+		index++;
+	return (index + 1);
+}
+
+void	string_array_free(char ***arr)
+{
+	char	**cursor;
+
+	if (!*arr)
+		return ;
+	cursor = *arr;
+	while (*cursor)
+		free(*(cursor++));
+	free(*arr);
+	*arr = NULL;
+}
+
+char	**string_array_dup(char **arr)
+{
+	char	**dup;
+	int		i;
+
+	if (!arr)
+		return (NULL);
+	dup = ft_calloc(string_array_len(arr) + 1, sizeof(*arr));
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (arr[i])
+	{
+		dup[i] = ft_strdup(arr[i]);
+		if (!dup[i])
+			return (string_array_free(&dup), NULL);
+		i++;
+	}
+	return (dup);
+}
+
+void	string_array_push(char ***arr, char *str)
+{
+	char	**dup;
+	int		i;
+
+	if (!str)
+		return ;
+	dup = ft_calloc(string_array_len(*arr) + 2, sizeof(**arr));
+	if (!dup)
+		return ;
+	i = 0;
+	while ((*arr)[i])
+	{
+		dup[i] = (*arr)[i];
+		i++;
+	}
+	dup[i] = str;
+	*arr = dup;
+	return ;
+}
+
+void	string_array_delete(char **arr, char *str)
+{
+	if (!str)
+		return ;
+	while (*arr && *arr != str)
+		arr++;
+	if (*arr != str)
+		return ;
+	while (*(arr + 1))
+	{
+		*arr = *(arr + 1);
+		arr++;
+	}
+	free(str);
+	*arr = NULL;
+}
---
+++ src/utils/string_array.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   string_array.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvoisard <jonas.voisard@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/21 20:01:31 by jvoisard          #+#    #+#             */
+/*   Updated: 2025/01/21 21:49:03 by jvoisard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRING_ARRAY_H
+# define STRING_ARRAY_H
+# include <unistd.h>
+# include <stdbool.h>
+# include "libft.h"
+
+int		string_array_len(char **arr);
+char	**string_array_dup(char **arr);
+void	string_array_free(char ***arr);
+void	string_array_push(char ***arr, char *str);
+void	string_array_delete(char **arr, char *str);
+char	*string_array_find(
+			char **arr,
+			char *pattern,
+			bool (*match)(char *, char *)
+			);
+char	*string_array_find_start_with(char **arr, char *pattern);
+
+#endif
