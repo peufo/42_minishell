0f744bfef70245a3c83c3dab81bb4e866cedacfc
DyoDlm <delamancha@noopy.>
2025-01-25 12:46:44 +0100

    bipboup

--- src/utils/track_function.c
+++ src/utils/track_function.c
@@ -2,5 +2,26 @@
 
 void 	track_origin(int func)
 {
-	(void)func;
+	int 	fd;
+	char 	*line;
+	char 	*search;
+	char 	*found;
+
+	printf("AAAAAAAAAAAAAAAAAAA\n");
+	fd = open("macro.txt", O_RDONLY);
+	if (fd < 0)
+		return ;
+	search = ft_itoa(func);
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		if (ft_strnstr(line, search, 80))
+			found = ft_strdup(line);
+		free(line);
+		break ;
+	}
+	free(line);
+	close(fd);
+	ft_putstr_fd(found, 1);
+	free(found);
+	free(search);
 }
