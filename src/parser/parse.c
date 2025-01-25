03d95850ec660518ec589777c247bb480e65c470
DyoDlm <delamancha@noopy.>
2025-01-25 15:00:43 +0100

    debug method fixed

--- src/parser/parse.c
+++ src/parser/parse.c
@@ -19,12 +19,23 @@ void	parse_free(t_sh *shell)
 
 void	parse(t_sh *shell)
 {
-	return ;
-	t_list	*tokens = shell->lexer.tokens;
+	t_token 	*token;
+	t_list 		*current_token;
+	t_list		*tokens = shell->lexer.tokens;
 
-	shell->parser.cmd->args = shell->lexer.tokens;
+	if (!tokens)
+		return ;
+	shell->parser.cmd->args = tokens;
 	shell->parser.cmd->pipe.in = STDIN_FILENO;
 	shell->parser.cmd->pipe.out = STDOUT_FILENO;
+	current_token = tokens;
+	while (current_token)
+	{
+		token = (t_token *)current_token->content;
+		if (token)
+			printf("Token : %s\n", token->value.value);
+		current_token = current_token->next;
+	}
 	if ((char *)shell->parser.cmd->args->content)
 	{
 		(void)tokens;
