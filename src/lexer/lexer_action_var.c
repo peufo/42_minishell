47dd2382520b048c76eb07b3c789da098ade9809
jonas <jonas.voisard@gmail.com>
2025-02-04 21:28:11 +0100

    use builtin env

--- src/lexer/lexer_action_var.c
+++ src/lexer/lexer_action_var.c
@@ -6,7 +6,7 @@
 /*   By: jvoisard <jonas.voisard@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 18:13:28 by jvoisard          #+#    #+#             */
-/*   Updated: 2025/02/04 19:27:46 by jvoisard         ###   ########.fr       */
+/*   Updated: 2025/02/04 21:15:30 by jvoisard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	lexer_action_expand_var(t_sh *shell)
 {
 	char	*value;
 
-	value = getenv(shell->lexer.varname.value);
+	value = get_env(shell, shell->lexer.varname.value);
 	string_free(&shell->lexer.varname);
 	if (value)
 		string_push_str(&shell->lexer.token, value);
