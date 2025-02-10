8c401945db563f8e56531b39a38f70ec784f3a34
DyoDlm <seitealexander@gmail.com>
2025-02-10 08:13:19 -0500

    last last

--- src/debug.c
+++ src/debug.c
@@ -6,7 +6,7 @@
 /*   By: dyodlm <dyodlm@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 16:27:29 by jvoisard          #+#    #+#             */
-/*   Updated: 2025/02/10 07:00:17 by dyodlm           ###   ########.fr       */
+/*   Updated: 2025/02/10 07:37:09 by dyodlm           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,7 +68,7 @@ void	debug_node(t_sh *shell, t_ast *node, int call)
 	int	i;
 
 	i = 0;
-	debug(shell, "TYPE IS :\n");
+	debug(shell, "TYPE IS : \n");
 	return (throw_error("NoNodeToDebug\n", __FILE__, __LINE__));
 	if (node->type == AST_COMMAND)
 	{
--- src/parser/parse_checkers.c
+++ src/parser/parse_checkers.c
@@ -30,31 +30,3 @@ void	check_list_content(t_sh *shell, t_list *lst)
 		node = node->next;
 	}
 }
-
-t_list	*pars_get_typelist(char **toks, int mod, t_sh *shell)
-{
-	t_list	*lst;
-	int		type;
-	int		i;
-
-	i = 0;
-	(void)shell;
-	lst = NULL;
-	type = pars_get_type(toks[i]);
-	while (toks[i] && type != AST_END)
-	{
-		if (mod == AST_COMMAND)
-		{
-			if (type == mod)
-				ft_lstadd_back(&lst, ft_lstnew(toks[i]));
-		}
-		else
-		{
-			if (type != AST_COMMAND)
-				ft_lstadd_back(&lst, ft_lstnew(toks[i]));
-		}
-		type = pars_get_type(toks[i++]);
-	}
-	debug(shell, "you got the list man\n");
-	return (lst);
-}
