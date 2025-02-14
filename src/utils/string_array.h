e11efce53ffdb7ce9bd13f650cb531d50830b8cc
Jonas <jonas.voisard@gmail.com>
2025-02-14 13:10:57 +0100

    utils: add string_array find last

--- src/utils/string_array.h
+++ src/utils/string_array.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   string_array.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jvoisard <jonas.voisard@gmail.com>         +#+  +:+       +#+        */
+/*   By: jvoisard <jvoisard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 20:01:31 by jvoisard          #+#    #+#             */
-/*   Updated: 2025/02/04 23:26:41 by jvoisard         ###   ########.fr       */
+/*   Updated: 2025/02/14 11:43:03 by jvoisard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,12 @@ char	**string_array_dup(char **arr);
 void	string_array_free(char ***arr);
 void	string_array_push(char ***arr, char *str);
 void	string_array_delete(char **arr, char *str);
-char	**string_array_find(
-			char **arr,
-			char *pattern,
-			bool (*match)(char *, char *)
-			);
 char	**string_array_find_start_with(char **arr, char *pattern);
+char	**string_array_find_match(char **arr, char *pattern);
+char	**string_array_find_last_start_with(char **arr, char *pattern);
+char	**string_array_find_last_match(char **arr, char *pattern);
+
+bool	string_match(char *pattern, char *str);
+bool	string_match_start_with(char *pattern, char *str);
 
 #endif
--- src/utils/string_array_find_last.c
+++ src/utils/string_array_find_last.c
@@ -1,38 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   string_array_find.c                                :+:      :+:    :+:   */
+/*   string_array_find_last.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jvoisard <jvoisard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 21:24:04 by jvoisard          #+#    #+#             */
-/*   Updated: 2025/02/14 11:43:51 by jvoisard         ###   ########.fr       */
+/*   Updated: 2025/02/14 11:43:34 by jvoisard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "string_array.h"
 
-static char	**string_array_find(
+static char	**string_array_find_last(
 	char **arr,
 	char *pattern,
 	bool (*match)(char *, char *)
 )
 {
-	while (*arr)
+	char	**cursor;
+
+	cursor = arr;
+	if (!*cursor)
+		return (NULL);
+	while (*cursor)
+		cursor++;
+	while (cursor >= arr)
 	{
-		if (match(pattern, *arr))
-			return (arr);
-		arr++;
+		if (match(pattern, *cursor))
+			return (cursor);
+		cursor--;
 	}
 	return (NULL);
 }
 
-char	**string_array_find_start_with(char **arr, char *pattern)
+char	**string_array_find_last_start_with(char **arr, char *pattern)
 {
 	return (string_array_find(arr, pattern, string_match_start_with));
 }
 
-char	**string_array_find_match(char **arr, char *pattern)
+char	**string_array_find_last_match(char **arr, char *pattern)
 {
 	return (string_array_find(arr, pattern, string_match));
 }
