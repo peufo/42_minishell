bd296b63b34ba9ac293d3daa0c3353c60c442551
DyoDlm <seitealexander@gmail.com>
2025-02-24 15:38:19 -0500

    fix bug redir

--- src/lex_eof/lex_eof_checker.c
+++ src/lex_eof/lex_eof_checker.c
@@ -6,7 +6,7 @@
 /*   By: dyodlm <dyodlm@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 06:59:20 by dyodlm            #+#    #+#             */
-/*   Updated: 2025/02/24 15:16:13 by dyodlm           ###   ########.fr       */
+/*   Updated: 2025/02/24 15:38:03 by dyodlm           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static bool	check_dquote(char *line, int state)
 
 	i = 0;
 	count = 0;
-	printf("state : %d\n", state);
+//	printf("state : %d\n", state);
 	if (state % 2 == 0)
 		count += 1;
 	while (line && line[i])
@@ -57,11 +57,11 @@ bool	check_end_in_line(char *line, int state, int type)
 {
 	if (!line)
 		return (false);
-	if (state == LEXER_DQUOTE || state == LEXER_VAR_DQUOTE)
+	if ((state == LEXER_DQUOTE || state == LEXER_VAR_DQUOTE) && type != 4)
 	{
 		if (*line == '"')
 			return (LINE_IS_COMPLETE);
-		printf("line computed with check dquote\n");
+//		printf("line computed with check dquote\n");
 		return (check_dquote(line, state));
 	}
 	else if (state == LEXER_QUOTE && type != 3)
