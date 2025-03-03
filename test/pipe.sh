f1bfb9ea22f0973f4a341035ff6b208e9b08641a
jonas <jonas.voisard@gmail.com>
2025-03-03 16:25:30 +0100

    handle comment line

--- test/pipe.sh
+++ test/pipe.sh
@@ -1,4 +1,4 @@
 echo "HEY" | grep "HE"
-cat Dockerfile | grep make
-ls -l | grep "drwxr-xr-x" | sort
-cat src/minishell.h | grep typedef | awk '{printf "type:\t%s\n", $3}'
+#cat Dockerfile | grep make
+#ls -l | grep "drwxr-xr-x" | sort
+#cat src/minishell.h | grep typedef | awk '{printf "type:\t%s\n", $3}'
