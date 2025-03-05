c6456478f5aa25260115c966eb48826c3dadb5e1
Jonas <jonas.voisard@gmail.com>
2025-03-05 18:08:44 +0100

    test: add basique test for status

--- test/status.sh
+++ test/status.sh
@@ -1,3 +1,6 @@
-#echo "EXIT WITH $? STATUS"
-#echo "exit_code ->$? user ->$USER home -> $HOME"
+echo "EXIT WITH $? STATUS"
+echo "exit_code ->$? user ->$USER home -> $HOME"
 echo $?HELLO
+ls /not_dir
+echo "EXIT WITH $? STATUS"
+echo "EXIT WITH $? STATUS"
