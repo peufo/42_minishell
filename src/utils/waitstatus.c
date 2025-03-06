c8fddfef613dccf92b1e1df283f917aa9795f40f
Jonas <jonas.voisard@gmail.com>
2025-03-06 10:30:13 +0100

    fix: waitpid() set a "wait status" not directly "exit status"

---
+++ src/utils/waitstatus.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   waitstatus.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvoisard <jvoisard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/06 10:24:44 by jvoisard          #+#    #+#             */
+/*   Updated: 2025/03/06 10:28:35 by jvoisard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	waitstatus(pid_t pid)
+{
+	int	wstatus;
+	int	status;
+
+	waitpid(pid, &wstatus, 0);
+	status = WEXITSTATUS(wstatus);
+	return (status);
+}
