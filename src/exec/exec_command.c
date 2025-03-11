7e221690f65d276a29c96f583772b5419187d2d9
jonas <jonas.voisard@gmail.com>
2025-03-11 23:19:51 +0100

    redirect leaks fd ok

--- src/exec/exec_command.c
+++ src/exec/exec_command.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   exec_command.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jvoisard <jvoisard@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jvoisard <jonas.voisard@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 12:47:50 by dyodlm            #+#    #+#             */
-/*   Updated: 2025/03/06 15:02:11 by jvoisard         ###   ########.fr       */
+/*   Updated: 2025/03/11 23:19:41 by jvoisard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	exec_redirect_open(t_ast *node)
+static void	exec_redirect_open(t_ast *node, int open_flags)
 {
 	char	**files;
 
@@ -22,24 +22,22 @@ static void	exec_redirect_open(t_ast *node)
 	files = node->redir.files_out;
 	while (*files)
 	{
-		node->redir.fd_out = open(*files, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+		node->redir.fd_out = open(*files, open_flags, 0666);
 		if (node->redir.fd_out == -1)
 			shell_exit(node->shell);
-		if (!(files))
-		{
-			close(node->redir.fd_out);
-		}
-		else
-			dup2(node->redir.fd_out, STDOUT_FILENO);
+		if (!*(files + 1))
+			if (dup2(node->redir.fd_out, STDOUT_FILENO) == -1)
+				shell_exit(node->shell);		
+		if (close(node->redir.fd_out) == -1)
+			shell_exit(node->shell);
 		files++;
 	}
 }
 
-static void	exec_redirect_close(t_ast *node)
+static void	exec_redir_restore_std(t_ast *node)
 {
 	if (!node->redir.files_out)
 		return ;
-	close(node->redir.fd_out);
 	dup2(node->redir.fd_std_out, STDOUT_FILENO);
 	close(node->redir.fd_std_out);
 }
@@ -50,13 +48,13 @@ int	exec_command(t_ast *node)
 	t_exe	builtin;
 
 	lex(node, node->line);
-	exec_redirect_open(node);
+	exec_redirect_open(node, O_WRONLY | O_CREAT | O_TRUNC);
 	builtin = get_builtin(*node->tokens);
 	if (builtin)
 	{
 		node->status = builtin(node);
 		node->shell->exit_status = node->status;
-		exec_redirect_close(node);
+		exec_redir_restore_std(node);
 		return (node->status);
 	}
 	if (node->is_child_process)
@@ -66,7 +64,7 @@ int	exec_command(t_ast *node)
 		return (node->status);
 	}
 	exec_child(node, exec_bin);
-	exec_redirect_close(node);
+	exec_redir_restore_std(node);
 	node->status = waitstatus(node->pid);
 	node->shell->exit_status = node->status;
 	return (node->status);
--- test/redirect.sh
+++ test/redirect.sh
@@ -12,3 +12,33 @@ grep . log/redirect/a.out log/redirect/b.out
 grep . log/redirect/a.out log/redirect/b.out
 > log/redirect/a.out  > log/redirect/b.out  echo "GGGG" > log/redirect/c.out 
 grep . log/redirect/a.out log/redirect/b.out log/redirect/c.out
+
+# echo "AAAA" >> log/redirect/a.out
+# grep . log/redirect/a.out
+# echo "BBBB" >> log/redirect/a.out >> log/redirect/b.out 
+# grep . log/redirect/a.out log/redirect/b.out
+# echo "CCCC" >>log/redirect/a.out >> log/redirect/b.out 
+# grep . log/redirect/a.out log/redirect/b.out
+# echo >>log/redirect/a.out "DDDD" >> log/redirect/b.out 
+# grep . log/redirect/a.out log/redirect/b.out
+# echo >> log/redirect/a.out "EEEE" >> log/redirect/b.out 
+# grep . log/redirect/a.out log/redirect/b.out
+# >> log/redirect/a.out echo "FFFF" >> log/redirect/b.out 
+# grep . log/redirect/a.out log/redirect/b.out
+# >> log/redirect/a.out  >> log/redirect/b.out  echo "GGGG" >> log/redirect/c.out 
+# grep . log/redirect/a.out log/redirect/b.out log/redirect/c.out
+
+# echo "AAAA" < log/redirect/a.out
+# grep . log/redirect/a.out
+# echo "BBBB" < log/redirect/a.out < log/redirect/b.out 
+# grep . log/redirect/a.out log/redirect/b.out
+# echo "CCCC" <log/redirect/a.out < log/redirect/b.out 
+# grep . log/redirect/a.out log/redirect/b.out
+# echo <log/redirect/a.out "DDDD" < log/redirect/b.out 
+# grep . log/redirect/a.out log/redirect/b.out
+# echo < log/redirect/a.out "EEEE" < log/redirect/b.out 
+# grep . log/redirect/a.out log/redirect/b.out
+# < log/redirect/a.out echo "FFFF" < log/redirect/b.out 
+# grep . log/redirect/a.out log/redirect/b.out
+# < log/redirect/a.out  < log/redirect/b.out  echo "GGGG" < log/redirect/c.out 
+# grep . log/redirect/a.out log/redirect/b.out log/redirect/c.out
