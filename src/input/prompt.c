ead6925ab15c561802023bc1a9ef4300b90cf695
jonas <jonas.voisard@gmail.com>
2025-03-12 23:44:15 +0100

    kali prompt

--- src/input/prompt.c
+++ src/input/prompt.c
@@ -6,7 +6,7 @@
 /*   By: jvoisard <jonas.voisard@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 21:07:25 by jvoisard          #+#    #+#             */
-/*   Updated: 2025/03/12 23:20:19 by jvoisard         ###   ########.fr       */
+/*   Updated: 2025/03/12 23:44:12 by jvoisard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,13 +39,15 @@ void	shell_update_prompt(t_sh *shell)
 		base_color = PROMPT_RED;
 	else
 		base_color = PROMPT_GREEN;
-	add(shell, "╭─ ", base_color);
+	add(shell, "╭─(", base_color);
 	add(shell, env_get(shell, "LOGNAME"), PROMPT_BLUE);
 	add(shell, "@", PROMPT_BLUE);
 	add(shell, env_get(shell, "HOSTNAME"), PROMPT_BLUE);
-	add(shell, " ┅ ", base_color);
-	add(shell, env_get(shell, "PWD"), PROMPT_PURPLE);
+	add(shell, ")─[", base_color);
+	add(shell, env_get(shell, "PWD"), NULL);
+	add(shell, "]", base_color);
 	add(shell, "\n", NULL);
-	add(shell, "╰─⟢ ", base_color);
+	add(shell, "╰─", base_color);
+	add(shell, "$ ", PROMPT_BLUE);
 	return ;
 }
