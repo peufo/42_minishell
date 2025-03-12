bab9e8cd2df8d42beafa26f059cb7505e7fc2604
jonas <jonas.voisard@gmail.com>
2025-03-12 18:46:41 +0100

    hide env vars without '='

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jvoisard <jvoisard@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jvoisard <jonas.voisard@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 23:15:32 by jvoisard          #+#    #+#             */
-/*   Updated: 2025/03/07 17:14:38 by jvoisard         ###   ########.fr       */
+/*   Updated: 2025/03/12 18:44:59 by jvoisard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,21 @@
 int	builtin_env(t_ast *node)
 {
 	char	**env;
+	char	*cursor;
 
 	if (!node->shell->env)
 		return (1);
 	env = node->shell->env;
 	while (*env)
 	{
-		ft_putstr(*env);
-		ft_putstr("\n");
+		cursor = *env;
+		while (*cursor && *cursor != '=')
+			cursor++;
+		if (*cursor == '=')
+		{
+			ft_putstr(*env);
+			ft_putstr("\n");
+		}
 		env++;
 	}
 	return (0);
--- test/builtin.sh
+++ test/builtin.sh
@@ -1,42 +1,46 @@
 env | grep -v "_=" | grep -v "LD_PRELOAD="
-
-echo "Salut $USER"
-echo "Salut Jonas"
-echo "Salut Alex"
-echo Y'O '$LOGNAME" "comment"$"va?
-
 echo
-echo -n
-echo -nnnn yo
-echo -n -n -n yoyo
-echo -n -nn -n -As  -n -n yoyo
-echo
+export PROUT
+export HAHA=
+env | grep -v "_=" | grep -v "LD_PRELOAD=" | sort
+
+# echo "Salut $USER"
+# echo "Salut Jonas"
+# echo "Salut Alex"
+# echo Y'O '$LOGNAME" "comment"$"va?
+
+# echo
+# echo -n
+# echo -nnnn yo
+# echo -n -n -n yoyo
+# echo -n -nn -n -As  -n -n yoyo
+# echo
 
 
-export MY_FLAG="-n"
-echo -n "$MY_FLAG" -n yoyo
-echo -n $MY_FLAG -nn -n -As  -n -n yoyo
-echo 1 I LOVE ''
-echo 2 I LOVE ""
-export LOVED
-echo 3  I LOVE " $LO"
-echo 4  I LOVE "$LO"
-echo 5 I LOVE "$"
-echo 6 I LOVE '$'
-echo 7 I LOVE $
-echo 8 I LOVE "$LOVED"
-echo -n 9 I LOVE "$LOVED"
-echo 10 I LOVE "$LOVED" -n
-echo 11 I LOVE -n "$LOVED"
-export LOVED="MONTAIN AND $HOME"
-echo "12 I LOVE $LOVED"
-export LOVED="JUST MONTAIN"
-echo 1"3 I LOVE $LOVED"
-export LOVED="JUST $HOME"
-echo 14" I LOVE $LOVED"
-export LOVED='JUST $HOME'
-echo 15 "I LOVE $LOVED"
-export LOVED='JUST "$HOME"'
-echo 16 "I LOVE $LOVED"
-unset LOVED
-echo 17 "I LOVE $LOVED"
+# export MY_FLAG="-n"
+# echo -n "$MY_FLAG" -n yoyo
+# echo -n $MY_FLAG -nn -n -As  -n -n yoyo
+# echo 1 I LOVE ''
+# echo 2 I LOVE ""
+# export LOVED
+# echo 3  I LOVE " $LO"
+# echo 4  I LOVE "$LO"
+# echo 5 I LOVE "$"
+# echo 6 I LOVE '$'
+# echo 7 I LOVE $
+# echo 8 I LOVE "$LOVED"
+# echo -n 9 I LOVE "$LOVED"
+# echo 10 I LOVE "$LOVED" -n
+# echo 11 I LOVE -n "$LOVED"
+# export LOVED="MONTAIN AND $HOME"
+# echo "12 I LOVE $LOVED"
+# export LOVED="JUST MONTAIN"
+# echo 1"3 I LOVE $LOVED"
+# export LOVED="JUST $HOME"
+# echo 14" I LOVE $LOVED"
+# export LOVED='JUST $HOME'
+# echo 15 "I LOVE $LOVED"
+# export LOVED='JUST "$HOME"'
+# echo 16 "I LOVE $LOVED"
+# unset LOVED
+# echo 17 "I LOVE $LOVED"
