f8c5add26c910db704acc531524f9eef4565f524
DyoDlm <seitealexander@gmail.com>
2025-03-14 13:19:45 -0400

    checking errors out

--- src/input/check_errors.c
+++ src/input/check_errors.c
@@ -6,7 +6,7 @@
 /*   By: dyodlm <dyodlm@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 09:08:40 by dyodlm            #+#    #+#             */
-/*   Updated: 2025/03/14 13:07:42 by dyodlm           ###   ########.fr       */
+/*   Updated: 2025/03/14 13:19:23 by dyodlm           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ static int	pipe_sentinel(char *line)
 		return (0);
 	while (line[i] && ft_isspace(line[i]))
 		i++;
-	if (line[i] && ft_isalnum(line[i]))
+	if ((line[i] && ft_isalnum(line[i])) || line[1] == '|' || line[i] == '(')
 		return (0);
 	return (1);
 }
@@ -51,23 +51,25 @@ bool	did_eye_of_sawron(t_sh *shell)
 	bool	indquote;
 	bool	insquote;
 	char	*cursor;
+	char	*head;
 
 	indquote = false;
 	insquote = false;
 	if (!shell->line)
 		return (true);
 	cursor = ft_strdup(shell->line);
+	head = cursor;
 	while (*cursor)
 	{
 		check_quotes(*cursor, &indquote, &insquote);
 		if (!indquote && !insquote)
 		{
 			if (pipe_sentinel(cursor))
-				return (help(shell, "Pipe"), true);
+				return (free(head), help(shell, "Pipe"), true);
 			if (redir_sentinel(cursor))
-				return (help(shell, "Redir"), true);
+				return (free(head), help(shell, "Redir"), true);
 		}
 		cursor++;
 	}
-	return (false);
+	return (free(head), false);
 }
