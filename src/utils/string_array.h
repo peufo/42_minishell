5a26a40bebeeed8d91f375f588e66a54e6437e47
Jonas <jonas.voisard@gmail.com>
2025-03-15 14:38:01 +0100

    try to fix wilds order

--- src/utils/string_array.h
+++ src/utils/string_array.h
@@ -6,7 +6,7 @@
 /*   By: jvoisard <jvoisard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 20:01:31 by jvoisard          #+#    #+#             */
-/*   Updated: 2025/03/15 12:37:58 by jvoisard         ###   ########.fr       */
+/*   Updated: 2025/03/15 14:32:06 by jvoisard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ void	string_array_free(char ***arr);
 void	string_array_push(char ***arr, char *str);
 void	string_array_delete(char **arr, char *str);
 void	string_array_sort(char **arr, int (cmp)(char*, char*));
+void	string_array_sort_alpha(char **arr);
 char	*string_array_join(char **arr, char *separator);
 int		*string_array_create_indexes(char **arr, int (cmp)(char*, char*));
 char	**string_array_find_start_with(char **arr, char *pattern);
