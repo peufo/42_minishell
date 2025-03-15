b36983230cfa32500172c8e01efd1b5f2969f459
jonas <jonas.voisard@gmail.com>
2025-03-15 11:06:40 +0100

    export use string_array_sort

--- test/builtin.sh
+++ test/builtin.sh
@@ -1,14 +1,9 @@
-######################################################
-##		GREP ET CAT DOIVENT ETRE TESTE A LA MAIN	##
-######################################################
-
 #env | grep -v "_=" | grep -v "LD_PRELOAD="
 echo
 export PROUT
 export HAHA=
-#env | grep -v "_=" | grep -v "LD_PRELOAD=" | sort
-
-#export | grep -v "LD_PRELOAD="
+env | grep -v "_=" | grep -v "LD_PRELOAD=" | sort
+export | grep -v "LD_PRELOAD=" | grep -b "_"
 
 echo "Salut $USER"
 echo "Salut Jonas"
@@ -21,7 +16,6 @@ echo -nnnn yo
 echo -n -n -n yoyo
 echo -n -nn -n -As  -n -n yoyo
 echo
-
 
 export MY_FLAG="-n"
 echo -n "$MY_FLAG" -n yoyo
--- test/wild.sh
+++ test/wild.sh
@@ -1,9 +1,13 @@
-echo *
-echo *e
-echo .*
-echo .* "HAHAHA\n" *e
-echo *e*
-echo *e*e
-echo *i*
-export DOT="."
-echo $DOT*i*
+export | grep -v "LD_PRELOAD=" | grep -b "_"
+# export | grep -v "LD_PRELOAD=" | grep -b "_"
+# echo *
+# echo *e
+# echo .*
+# echo .* "HAHAHA\n" *e
+# echo *e*
+# echo *e*e
+# echo *i*
+# export DOT="."
+# echo $DOT*i*
+
+# ls *o*
