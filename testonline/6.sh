ab4ea7c3b2072a46be3dfc818c7b95d2d7f566b4
Jonas <jonas.voisard@gmail.com>
2025-03-19 15:33:13 +0100

    test online 6 OK

--- testonline/6.sh
+++ testonline/6.sh
@@ -2,6 +2,23 @@
 # #                                  CORRECTION                                  #
 # # **************************************************************************** #
 
+cd log
+
+mkdir tmp_path_test1 tmp_path_test2
+printf '#include <unistd.h>\nint main(){write(1, \"1\\n\", 2);}' > tmp_path_test1/test1.c
+gcc tmp_path_test1/test1.c -o tmp_path_test1/a.out
+printf '#include <unistd.h>\nint main(){write(1, \"2\\n\", 2);}' > tmp_path_test2/test2.c
+gcc tmp_path_test2/test2.c -o tmp_path_test2/a.out
+export PATH=tmp_path_test1:tmp_path_test2
+a.out
+export PATH=tmp_path_test2:tmp_path_test1
+a.out
+/bin/rm -rf tmp_path_test1 tmp_path_test2
+
+
+
+
+
 /bin/echo
 /bin/echo 1
 /bin/cat 42
@@ -239,26 +256,14 @@ unset PATH
 cd /bin
 ls
 
-# unset PATH
-# cd /bin/../bin/
-# ls
+unset PATH
+cd /bin/../bin/
+ls
 
-# unset PATH
-# cd /bin/../bin/../bin/../bin/../bin/../bin/../bin/../bin/
-# ../bin/../bin/ls
+unset PATH
+cd /bin/../bin/../bin/../bin/../bin/../bin/../bin/../bin/
+../bin/../bin/ls
 
-# unset PATH
-# cd /bin/
-# sleep 2
-
-# mkdir tmp_path_test1 tmp_path_test2
-# printf '#include <unistd.h>\nint main(){write(1, \"1\\n\", 2);}' > tmp_path_test1/test1.c
-# gcc tmp_path_test1/test1.c -o tmp_path_test1/a.out
-# printf '#include <unistd.h>\nint main(){write(1, \"2\\n\", 2);}' > tmp_path_test2/test2.c
-# gcc tmp_path_test2/test2.c -o tmp_path_test2/a.out
-# export PATH=tmp_path_test1:tmp_path_test2
-# a.out
-# export PATH=tmp_path_test2:tmp_path_test1
-# a.out
-# /bin/rm -rf tmp_path_test1 tmp_path_test2
-
+unset PATH
+cd /bin/
+sleep 2
