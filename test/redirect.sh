8ba64197e14ad1f13dba3defe6391bc98538668f
Jonas <jonas.voisard@gmail.com>
2025-03-25 11:21:05 +0100

    enable redirect.sh

--- test/redirect.sh
+++ test/redirect.sh
@@ -1,40 +1,40 @@
 export FILE_A="log/redirect/a.out"
 export FILE_B="log/redirect/b.out"
-# export FILE_C="log/redirect/c.out"
+export FILE_C="log/redirect/c.out"
 
-# echo "AAAA" > $FILE_A
-# grep . $FILE_A
+echo "AAAA" > $FILE_A
+grep . $FILE_A
 echo "BBBB" > $FILE_A > $FILE_B
-# grep . $FILE_A log/redirect/b.out
-# echo "CCCC" >$FILE_A > $FILE_B
-# grep . $FILE_A log/redirect/b.out
-# echo >$FILE_A "DDDD" > $FILE_B
-# grep . $FILE_A log/redirect/b.out
-# echo > $FILE_A "EEEE" > $FILE_B
-# grep . $FILE_A log/redirect/b.out
-# > $FILE_A echo "FFFF" > $FILE_B
-# grep . $FILE_A log/redirect/b.out
-# > $FILE_A  > $FILE_B echo "GGGG" > $FILE_C 
-# grep . $FILE_A $FILE_B $FILE_C
+grep . $FILE_A log/redirect/b.out
+echo "CCCC" >$FILE_A > $FILE_B
+grep . $FILE_A log/redirect/b.out
+echo >$FILE_A "DDDD" > $FILE_B
+grep . $FILE_A log/redirect/b.out
+echo > $FILE_A "EEEE" > $FILE_B
+grep . $FILE_A log/redirect/b.out
+> $FILE_A echo "FFFF" > $FILE_B
+grep . $FILE_A log/redirect/b.out
+> $FILE_A  > $FILE_B echo "GGGG" > $FILE_C 
+grep . $FILE_A $FILE_B $FILE_C
 
-# export FILE_A="log/redirect/d.out"
-# export FILE_B="log/redirect/e.out"
-# export FILE_C="log/redirect/f.out"
+export FILE_A="log/redirect/d.out"
+export FILE_B="log/redirect/e.out"
+export FILE_C="log/redirect/f.out"
 
-# grep o < Dockerfile > $FILE_B > $FILE_A > $FILE_C
-# cat < $FILE_A
-# grep o < $FILE_A
-# grep . $FILE_A $FILE_B
-# echo "YOLO" > $FILE_A >> $FILE_B 
-# grep . $FILE_A $FILE_B
-# echo "YALA" > $FILE_A >> $FILE_B >> $FILE_C
-# grep . $FILE_A $FILE_B $FILE_C
-# echo "HOHO" >> $FILE_A
-# grep . $FILE_A $FILE_B $FILE_C
-# grep "=" < Makefile > $FILE_A >> $FILE_B
-# grep . $FILE_A $FILE_B $FILE_C
-# grep "=" < Makefile >> $FILE_A > $FILE_B
-# grep . $FILE_A $FILE_B $FILE_C
-# grep "=" < dev.sh > $FILE_B >> $FILE_A
-# grep . $FILE_A $FILE_B $FILE_C
+grep o < Dockerfile > $FILE_B > $FILE_A > $FILE_C
+cat < $FILE_A
+grep o < $FILE_A
+grep . $FILE_A $FILE_B
+echo "YOLO" > $FILE_A >> $FILE_B 
+grep . $FILE_A $FILE_B
+echo "YALA" > $FILE_A >> $FILE_B >> $FILE_C
+grep . $FILE_A $FILE_B $FILE_C
+echo "HOHO" >> $FILE_A
+grep . $FILE_A $FILE_B $FILE_C
+grep "=" < Makefile > $FILE_A >> $FILE_B
+grep . $FILE_A $FILE_B $FILE_C
+grep "=" < Makefile >> $FILE_A > $FILE_B
+grep . $FILE_A $FILE_B $FILE_C
+grep "=" < dev.sh > $FILE_B >> $FILE_A
+grep . $FILE_A $FILE_B $FILE_C
 
