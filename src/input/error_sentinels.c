073fce32a12c7d400e51e5804b15a2706190b713
DyoDlm <seitealexander@gmail.com>
2025-03-27 11:03:12 -0400

    At least no syntax errors

--- src/input/error_sentinels.c
+++ src/input/error_sentinels.c
@@ -6,7 +6,7 @@
 /*   By: dyodlm <dyodlm@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 15:41:11 by dyodlm            #+#    #+#             */
-/*   Updated: 2025/03/27 09:15:35 by dyodlm           ###   ########.fr       */
+/*   Updated: 2025/03/27 10:58:40 by dyodlm           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,16 @@ bool	did_less_sentinel_see(char *line, bool in_eof)
 		return (false);
 	if (line[1] == '<')
 	{
+		line++;
 		while (ft_isspace(line[i]))
 			i++;
+		if (ft_isalnum(line[i]))
+			return (false);
 		if (!line[i] || line[i] == '<' || line[i] == '&' || line[i] == '|'
 			|| line[i] == '>')
 			return (true);
 	}
-	return (false);
+	return (!is_token_valid(line, "<", in_eof));
 }
 
 bool	did_great_sentinel_see(char *line, bool in_eof)
@@ -48,21 +51,20 @@ bool	did_great_sentinel_see(char *line, bool in_eof)
 	int	i;
 
 	i = 1;
-	(void)in_eof;
 	if (!line[0] || line[0] != '>')
 		return (false);
 	while (line[i] && ft_isspace(line[i]))
 		i++;
 	if (line[i] && ft_isalnum(line[i]))
 		return (false);
-	return (false);
+	return (!is_token_valid(line, ">", in_eof));
 }
 
 bool	did_other_sentinel_see(char *line, bool in_eof)
 {
 	if (!line[0] || line[0] != ';')
 		return (false);
-	return (is_token_valid(line, ":", in_eof));
+	return (!is_token_valid(line, ":", in_eof));
 }
 
 bool	did_logical_sentinel_see(char *line, bool in_eof)
