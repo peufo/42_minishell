5403f213fcf311555f4a3eabced1d828ce3bffc6
DyoDlm <seitealexander@gmail.com>
2025-03-28 11:08:06 -0400

    Ctrl-D whaaaat ?

--- src/shell/shell_exec.c
+++ src/shell/shell_exec.c
@@ -6,14 +6,16 @@
 /*   By: dyodlm <dyodlm@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 10:42:39 by dyodlm            #+#    #+#             */
-/*   Updated: 2025/03/28 10:33:39 by dyodlm           ###   ########.fr       */
+/*   Updated: 2025/03/28 10:52:26 by dyodlm           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	basic_exec(t_sh *shell)
+static void	basic_exec(t_sh *shell, int exec)
 {
+	if ((exec == 1 && !lex_eof(shell)) || did_eye_of_sawron(shell, 0))
+		return (shell_exec(shell));
 	if (count_redir_in_line(shell, shell->line, 0, 0))
 		treat_redirections(&shell->input, shell);
 	shell->ast = ast_create(shell, ft_strdup(shell->line));
@@ -38,14 +40,12 @@ void	shell_exec(t_sh *shell)
 	{
 		g_signal.is_sigint = false;
 		exec = input_read(shell);
-		if ((exec == 1 && !lex_eof(shell)) || did_eye_of_sawron(shell, 0))
-			return (shell_exec(shell));
 		if (!shell->line && !shell->is_interactive)
 			shell_exit(shell);
 		if (*shell->line == '#')
 			continue ;
 		if (shell->line)
-			basic_exec(shell);
+			basic_exec(shell, exec);
 	}
 	shell_exit(shell);
 }
