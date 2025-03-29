6384543e2d0f6238f33f8214a3f261cc5351b312
jonas <jonas.voisard@gmail.com>
2025-03-29 22:49:01 +0000

    fix: PATH order test

--- test/6.sh
+++ test/6.sh
@@ -5,9 +5,11 @@
 cd log
 
 mkdir tmp_path_test1 tmp_path_test2
-printf '#include <unistd.h>\nint main(){write(1, \"1\\n\", 2);}' > tmp_path_test1/test1.c
+echo '#include <unistd.h>' > tmp_path_test1/test1.c
+echo 'int main(){write(1, "1\n", 2);}' >> tmp_path_test1/test1.c
 gcc tmp_path_test1/test1.c -o tmp_path_test1/a.out
-printf '#include <unistd.h>\nint main(){write(1, \"2\\n\", 2);}' > tmp_path_test2/test2.c
+echo '#include <unistd.h>' > tmp_path_test2/test2.c
+echo 'int main(){write(1, "2\n", 2);}' >> tmp_path_test2/test2.c
 gcc tmp_path_test2/test2.c -o tmp_path_test2/a.out
 export PATH=tmp_path_test1:tmp_path_test2
 a.out
@@ -15,250 +17,250 @@ export PATH=tmp_path_test2:tmp_path_test1
 a.out
 /bin/rm -rf tmp_path_test1 tmp_path_test2
 
-/bin/echo
-/bin/echo 1
-/bin/cat 42
+# /bin/echo
+# /bin/echo 1
+# /bin/cat 42
 
-""
+# ""
 
-"   "
+# "   "
 
-"    "
+# "    "
 
-"		"
+# "		"
 
-"			"
+# "			"
 
-/bin/echo hello 42
+# /bin/echo hello 42
 
-/bin/sleep 0
+# /bin/sleep 0
 
-/bin/echo -n 1
+# /bin/echo -n 1
 
-echo
+# echo
 
-echo hello world
+# echo hello world
 
-echo -n 42
+# echo -n 42
 
-echo -n 42 -n
+# echo -n 42 -n
 
-echo 42 -n
+# echo 42 -n
 
-echo -n -n -n 42
+# echo -n -n -n 42
 
-echo -n -n -n -n 42
+# echo -n -n -n -n 42
 
-/bin/echo 42
-echo $?
+# /bin/echo 42
+# echo $?
 
-/bin/cat 42
-echo $?
+# /bin/cat 42
+# echo $?
 
-/bin/exe42 42
+# /bin/exe42 42
 
-42 42
+# 42 42
 
-/bin/echo 42
-expr $? + $?
+# /bin/echo 42
+# expr $? + $?
 
-/bin/cat 42
-expr $? + $?
+# /bin/cat 42
+# expr $? + $?
 
-echo "Hello World"
+# echo "Hello World"
 
-echo "  ""Hello World"
+# echo "  ""Hello World"
 
-echo "  "Hello "World "
+# echo "  "Hello "World "
 
-"echo" 42
+# "echo" 42
 
-"echo 42"
+# "echo 42"
 
-echo " cat ' lol.c"
+# echo " cat ' lol.c"
 
-/bin/echo @World"42"Hello
+# /bin/echo @World"42"Hello
 
-echo 'Hello World'
+# echo 'Hello World'
 
-echo '  ' Hello World
+# echo '  ' Hello World
 
-echo '  'Hello 'World '
+# echo '  'Hello 'World '
 
-'echo' 42
+# 'echo' 42
 
-'e'cho 42
+# 'e'cho 42
 
-echo 'cat lol.c << cat > lol.c'
+# echo 'cat lol.c << cat > lol.c'
 
-echo @World'42'Hello
+# echo @World'42'Hello
 
-echo "" ''
+# echo "" ''
 
-echo """"''""''""
+# echo """"''""''""
 
-echo " "" " ' ' " " ' ' " "
+# echo " "" " ' ' " " ' ' " "
 
-echo 1 '|' echo 2
+# echo 1 '|' echo 2
 
-echo 1 '&&' echo 2
+# echo 1 '&&' echo 2
 
-echo 1 '||' echo 2
+# echo 1 '||' echo 2
 
-echo 1 '>' echo 2
+# echo 1 '>' echo 2
 
-echo 1 '>>' file 2
+# echo 1 '>>' file 2
 
-'<<' lim echo 1 >>/dev/null
+# '<<' lim echo 1 >>/dev/null
 
-echo "$USER"
+# echo "$USER"
 
-echo "   $   "
+# echo "   $   "
 
-echo '$USER'
+# echo '$USER'
 
-echo '   $   '
+# echo '   $   '
 
-echo "$HO ME"
+# echo "$HO ME"
 
-echo '$HO ME'
+# echo '$HO ME'
 
-echo $"HOME"
+# echo $"HOME"
 
-echo $'HOME'
+# echo $'HOME'
 
-echo ""$PWD""
+# echo ""$PWD""
 
-echo ''$PWD''
+# echo ''$PWD''
 
-echo $USE"R"$HOME
+# echo $USE"R"$HOME
 
-echo $"HOME"$USER
+# echo $"HOME"$USER
 
-echo $"HOM"E$USER
+# echo $"HOM"E$USER
 
-echo $"'HOM'E"$USER
+# echo $"'HOM'E"$USER
 
-echo $'HOM'E$USER
+# echo $'HOM'E$USER
 
-echo $"HOME"
+# echo $"HOME"
 
-echo $'HOME'
+# echo $'HOME'
 
-echo $
+# echo $
 
-echo "$"
+# echo "$"
 
-echo '$='
+# echo '$='
 
-echo "$""$"
+# echo "$""$"
 
-echo $"$"
+# echo $"$"
 
-echo $"42$"
+# echo $"42$"
 
-echo "$"$
+# echo "$"$
 
-echo $'$'
+# echo $'$'
 
-echo '$'$
+# echo '$'$
 
-export USER="@?/"
-env | grep USER | grep -v USER_ZDOTDIR
+# export USER="@?/"
+# env | grep USER | grep -v USER_ZDOTDIR
 
-export NEW="4 2"
-env | grep NEW | grep -v GLIB
+# export NEW="4 2"
+# env | grep NEW | grep -v GLIB
 
-export NEW="4=2"
-env | grep NEW | grep -v GLIB
+# export NEW="4=2"
+# env | grep NEW | grep -v GLIB
 
-export NEW=4=2
-env | grep 4=2
-export =
+# export NEW=4=2
+# env | grep 4=2
+# export =
 
-export ''=''
+# export ''=''
 
-export ""=""
+# export ""=""
 
-export export
+# export export
 
-export =============123
+# export =============123
 
-export echo
+# export echo
 
-unset
+# unset
 
-unset env
+# unset env
 
-unset PATH
-echo $PATH
-unset PATH
-echo $PATH
-unset HOME
-echo $HOME
+# unset PATH
+# echo $PATH
+# unset PATH
+# echo $PATH
+# unset HOME
+# echo $HOME
 
-export NEW=42
-env | grep NEW | grep -v GLIB
-unset NEW
-env | grep NEW | grep -v GLIB
-unset NEW
-unset NEW
-unset NEW
-unset NEW
-unset NEW
-env | grep NEW
+# export NEW=42
+# env | grep NEW | grep -v GLIB
+# unset NEW
+# env | grep NEW | grep -v GLIB
+# unset NEW
+# unset NEW
+# unset NEW
+# unset NEW
+# unset NEW
+# env | grep NEW
 
-cd .
-/bin/ls
+# cd .
+# /bin/ls
 
-cd ..
-/bin/ls
+# cd ..
+# /bin/ls
 
-cd /notsuchdir
-/bin/ls
+# cd /notsuchdir
+# /bin/ls
 
-cd /////
-/bin/ls
+# cd /////
+# /bin/ls
 
-cd "cmds/"
-/bin/ls
+# cd "cmds/"
+# /bin/ls
 
-cd cmds/
-/bin/ls
+# cd cmds/
+# /bin/ls
 
-cd ./cmds/
-/bin/ls
+# cd ./cmds/
+# /bin/ls
 
-mkdir bla_test
-chmod -wrx bla_test
-cd bla_test
-chmod +wrx bla_test
-rm -r bla_test
+# mkdir bla_test
+# chmod -wrx bla_test
+# cd bla_test
+# chmod +wrx bla_test
+# rm -r bla_test
 
-pwd
+# pwd
 
-cd $HOME
-pwd
+# cd $HOME
+# pwd
 
-pwd pwd
+# pwd pwd
 
-pwd hello
+# pwd hello
 
-unset PATH
-pwd
+# unset PATH
+# pwd
 
-unset PATH
-cd /bin
-ls
+# unset PATH
+# cd /bin
+# ls
 
-unset PATH
-cd /bin/../bin/
-ls
+# unset PATH
+# cd /bin/../bin/
+# ls
 
-unset PATH
-cd /bin/../bin/../bin/../bin/../bin/../bin/../bin/../bin/
-../bin/../bin/ls
+# unset PATH
+# cd /bin/../bin/../bin/../bin/../bin/../bin/../bin/../bin/
+# ../bin/../bin/ls
 
-unset PATH
-cd /bin/
-sleep 2
+# unset PATH
+# cd /bin/
+# sleep 2
