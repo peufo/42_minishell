ad271b421f9c62945022d94b4d0f58d7af404ac5
jonas <jonas.voisard@gmail.com>
2025-03-29 22:32:59 +0000

    test 4 100% OK

--- test/todo.sh
+++ test/todo.sh
@@ -1,4 +1,10 @@
 
+# Don't log std err on non interactive mode
+# ls > log/out >> log/out1 >log/out3 | cat log/out1
+# /bin/rm -f log/out
+# /bin/rm -f log/out1
+# /bin/rm -f log/out3
+
 ## EXPORT APPEND
 # export TEST+=100
 # export TEST+=100
