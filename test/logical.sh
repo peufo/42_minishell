3067f1897d8bd08c6c5e4a422c774b677405941b
jonas <jonas.voisard@gmail.com>
2025-03-29 19:43:58 +0000

    fix: export bad identifier error

--- test/logical.sh
+++ test/logical.sh
@@ -1,3 +1,4 @@
 ls /not_dir && ls /usr || ls /not_dir || ls /usr
 (ls /not_dir && ls /usr || ls /not_dir) || ls /usr
 ls /not_dir && ls /usr || (ls /not_dir && ls /not_dir || ls /etc) && ls /usr
+(export VAR=TOTO && echo "var in subshell = '$VAR'") && echo "var out subshell = '$VAR'"
