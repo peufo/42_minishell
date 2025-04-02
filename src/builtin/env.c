81213e4ef99f97356770f652da69cfb543a936eb
Jonas <jonas.voisard@gmail.com>
2025-04-02 12:20:35 +0200

    fix: unset varnam without "="

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -6,7 +6,7 @@
 /*   By: jvoisard <jvoisard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 23:15:32 by jvoisard          #+#    #+#             */
-/*   Updated: 2025/03/17 17:48:52 by jvoisard         ###   ########.fr       */
+/*   Updated: 2025/04/02 12:17:41 by jvoisard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,8 @@ static char	**get_env_ptr(t_sh *shell, char *varname)
 	ft_strlcat(key, "=", len);
 	arr_ptr = string_array_find_start_with(shell->env, key);
 	free(key);
+	if (!arr_ptr)
+		arr_ptr = string_array_find_start_with(shell->env, varname);
 	return (arr_ptr);
 }
 
