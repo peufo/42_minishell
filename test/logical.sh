76f43f1a0ff7d2ade7b73a3d1c778a4314d9d26f
Jonas <jonas.voisard@gmail.com>
2025-04-02 14:13:25 +0200

    uncomment test logical

--- test/logical.sh
+++ test/logical.sh
@@ -1,5 +1,4 @@
-# ls /not_dir && ls /usr || ls /not_dir || ls /usr
-# (ls /not_dir && ls /usr || ls /not_dir) || ls /usr
-# ls /not_dir && ls /usr || (ls /not_dir && ls /not_dir || ls /etc) && ls /usr
-# (export VAR=TOTO && echo "var in subshell = '$VAR'") && echo "var out subshell = '$VAR'"
-| echo hoho | grep 2
+ls /not_dir && ls /usr || ls /not_dir || ls /usr
+(ls /not_dir && ls /usr || ls /not_dir) || ls /usr
+ls /not_dir && ls /usr || (ls /not_dir && ls /not_dir || ls /etc) && ls /usr
+(export VAR=TOTO && echo "var in subshell = '$VAR'") && echo "var out subshell = '$VAR'"
